f3d381adbfb271eb4ef710d68a0b0e2b86ca3f6c
EduardoCGarcia <echavezg004@gmail.com>
2021-11-17 07:31:11 -0600

    generar pdf

--- FacturasXmlPdf/pdfGenerator.py
+++ FacturasXmlPdf/pdfGenerator.py
@@ -39,25 +39,34 @@ class PDFOrder(object):
         styles = getSampleStyleSheet()
         xml = self.xml_obj
         
-        address = """ <font size="9">
+        address = """ <font size="12">
+        <br/>
         Enviar a:<br/>
         <br/>
         %s<br/>
+        <br/>
         %s<br/>
+        <br/>
+        %s<br/>
+        <br/>
+        Código Postal: %s<br/>
         </font>
-        """ % (xml.factura_id,xml.nombre_cliente)
+        """ % (str(xml.nombre_cliente) + str(xml.apellido_paterno) + str(xml.apellido_materno),
+        xml.fecha_compra,
+        xml.direccion,xml.cp)
         p = Paragraph(address, styles["Normal"])
         p.wrapOn(self.canvas, width, self.height)
-        p.drawOn(self.canvas, *self.coord(18, 40, mm))
+        p.drawOn(self.canvas, *self.coord(18, 65, mm))
         
-        order_number = '<font size="14"><b>Factura #%s </b></font>' % xml.factura_id
-        p = Paragraph(order_number, styles["Normal"])
+        factura = '<font size="20"><b>Factura #%s </b></font>' % xml.factura_id
+        p = Paragraph(factura, styles["Normal"])
         p.wrapOn(self.canvas, width, self.height)
-        p.drawOn(self.canvas, *self.coord(18, 50, mm))
+        p.drawOn(self.canvas, *self.coord(18, 20, mm))
         
         data = []
         data.append(["ISBN","Libro", "Precio", "Cantidad", "Total"])
         grand_total = 0
+        total_libros = 0
         for libro in xml.compra.iterchildren():
             row = []
             row.append(libro.isbn)
@@ -65,22 +74,23 @@ class PDFOrder(object):
             row.append(libro.precio)
             row.append(libro.cantidad)
             total = Decimal(str(libro.precio)) * Decimal(str(libro.cantidad))
+            total_libros += Decimal(str(libro.cantidad))
             row.append(str(total))
             grand_total += total
             data.append(row)
-        data.append(["", "", "", "Grand Total:", grand_total])
+        data.append(["", "", "TOTALES", total_libros, grand_total])
         t = Table(data, 1.5 * inch)
         t.setStyle(TableStyle([
             ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
             ('BOX', (0,0), (-1,-1), 0.25, colors.black)
         ]))
         t.wrapOn(self.canvas, width, self.height)
-        t.drawOn(self.canvas, *self.coord(18, 85, mm))
+        t.drawOn(self.canvas, *self.coord(18, 150, mm))
         
-        txt = "Thank you for your business!"
+        txt = "GRACIAS POR SU COMPRA!"
         p = Paragraph(txt, styles["Normal"])
         p.wrapOn(self.canvas, width, self.height)
-        p.drawOn(self.canvas, *self.coord(18, 95, mm))
+        p.drawOn(self.canvas, *self.coord(18, 200, mm))
         
     #----------------------------------------------------------------------
     def getXMLObject(self):
